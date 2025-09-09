5ed808fae539caeb31aef9a318667f2e954a9d31
bethq321 <bethqualls@comcast.net>
2025-09-09 16:32:31 -0500

    create gameboard class

--- script.js
+++ script.js
@@ -13,15 +13,78 @@ class Ship {
     }
 }
 
-const carrier = new Ship(5);
-const battleship = new Ship(4);
-const destroyer = new Ship(3);
-const submarine = new Ship(3);
-const patrolBoat = new Ship(2);
+// const carrier = new Ship(5);
+// const battleship = new Ship(4);
+// const destroyer = new Ship(3);
+// const submarine = new Ship(3);
+// const patrolBoat = new Ship(2);
 
-function hitShip(ship) {
-    ship.hit();
-    return ship.timesHit;
+class Gameboard {
+    constructor() {
+        this.length = 10;
+        this.width = 10;
+        this.board = this.generateBoard(this.length, this.width);
+    }
+
+    generateBoard(length, width) {
+        const newBoard = [];
+        for(let i = 0; i < length; i++) {
+            newBoard[i] = [];
+            for(let j = 0; j < width; j++) {
+                newBoard[i][j] = 'e';
+            }
+        }
+        return newBoard;
+    }
+
+    placeShip(coordinate, ship, orientation) {
+        const [x, y] = coordinate;
+
+        // check boundaries
+        if(orientation === 'h') {
+            if(x + ship.length > this.width) {
+                return false;
+            }
+        } else if(orientation === 'v') {
+            if(y + ship.length > this.length) {
+                return false;
+            }
+        };
+        
+        // check collisions
+        if(orientation === 'h') {
+            for(let i = 0; i < ship.length; i++) {
+                if(this.board[x + i][y] !== 'e') {
+                    return false;
+                };
+            };
+        };
+
+        if(orientation === 'v') {
+            for(let i = 0; i < ship.length; i++) {
+                if(this.board[x][y + i] !== 'e') {
+                    return false;
+                };
+            };
+        };
+
+        // place ship
+        if(orientation === 'h') {
+            for(let i = 0; i < ship.length; i++) {
+                this.board[x + i][y] = ship;
+            };
+        };
+
+        if(orientation === 'v') {
+            for(let i = 0; i < ship.length; i++) {
+                this.board[x][y + i] = ship;
+            };
+        };
+
+
+        return true;
+    }
+
 }
 
-export { Ship, hitShip }
+export { Ship, Gameboard }
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { Ship, hitShip } from "./script";
+import { Ship, Gameboard } from "./script";
 
 // const carrier = new Ship(5);
 // const battleship = new Ship(4);
@@ -6,16 +6,96 @@ import { Ship, hitShip } from "./script";
 // const submarine = new Ship(3);
 // const patrolBoat = new Ship(2);
 
-test('hit from 0 to 1', () => {
-    const carrier = new Ship(5);
-    expect(hitShip(carrier)).toBe(1);
+// Creates a new ship for each test
+let ship;
+beforeEach(() => {
+    ship = new Ship(3);
+});
+
+// Initial tests of the class
+test('confirm initial hits is 0', () => {
+    expect(ship.timesHit).toBe(0);
+});
+
+test('confirm initial isSunk state is false', () => {
+    expect(ship.isSunk()).toBe(false);
+});
+
+// Single hit
+test('timesHit increases from 0 to 1', () => {
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+});
+
+test('confirm isSunk is false after a single hit', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+});
+
+// Exactly sunk
+test('ship is sunk when hits = length', () => {
+    for(let i = 0; i < 3; i++) {
+        ship.hit();
+    }
+    expect(ship.isSunk()).toBe(true);
+});
+
+// Overkill
+test('ship is still sunk when hits > length', () => {
+    for(let i = 0; i < 4; i++) {
+        ship.hit();
+    }
+    expect(ship.isSunk()).toBe(true);
+});
+
+// Gameboard testing
+beforeEach(() => {
+    gameboard = new Gameboard();
+    ship1 = new Ship(3);
+    ship2 = new Ship(4);
 })
 
-test('ship is sunk', () => {
-    const length = 4;
-    const battleship = new Ship(length);
-    for(let i = 0; i < length; i++) {
-        hitShip(battleship);
-    }
-    expect(battleship.isSunk()).toBe(true);
+test('creates a gameboard with correct dimensions', () => {
+    expect(gameboard.board.length).toBe(10);
+    gameboard.board.forEach(row => {
+        expect(row.length).toBe(10);
+    })
+});
+
+test('initializes the board with spaces', () => {
+    gameboard.board.forEach(row => {
+        row.forEach(cell => {
+            expect(cell).toBe('e');
+        })
+    })
+});
+
+test('allows ship to fit exactly horizontally', () => {
+    expect(gameboard.placeShip([7, 7], ship1, 'h')).toBe(true);
+});
+
+test('prevents ship from horizontal overflow', () => {
+    expect(gameboard.placeShip([8, 7], ship1, 'h')).toBe(false);
+});
+
+test('allows ship to fit exactly vertically', () => {
+    expect(gameboard.placeShip([7, 7], ship1, 'v')).toBe(true);
+});
+
+test('prevents ship from vertical overflow', () => {
+    expect(gameboard.placeShip([7, 8], ship1, 'v')).toBe(false);
+});
+
+test('checks for correct placement of ships', () => {
+    gameboard.placeShip([0, 0], ship1, 'h');
+    expect(gameboard.board[0][0]).toBe(ship1);
+    expect(gameboard.board[1][0]).toBe(ship1);
+    expect(gameboard.board[2][0]).toBe(ship1);
+    expect(gameboard.board[3][0]).toBe('e');
+    expect(gameboard.board[0][1]).toBe('e');
+})
+
+test('prevents ships from collisions', () => {
+    gameboard.placeShip([0, 0], ship1, 'h');
+    expect(gameboard.placeShip([1, 0], ship2, 'h')).toBe(false);
 })
